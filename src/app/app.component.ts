bd241290ed72fc7d4694da170d1603702f071c3b
Jerome Desouches <jerome.desouches@gmail.com>
2024-04-02 16:38:09 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NasaService } from './services/nasa.service';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,18 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'nasaChallenge';
+
+  constructor(private nasaService: NasaService) {
+    this.getImageOfTheDay()
+  }
+
+  imageOfTheDay: string = ""
+
+  getImageOfTheDay(){
+    this.nasaService.getImageOfTheDay().subscribe(data => {
+      this.imageOfTheDay = data
+      console.log("imag nasa", data);
+      
+    })
+  }
 }
---
+++ src/app/services/nasa.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NasaService {
+
+  constructor(private http: HttpClient) { }
+
+  getImageOfTheDay(): Observable<string>{
+    return this.http.get<any>('https://api.nasa.gov/planetary/apod?api_key=uPwfYwI8qPWc9lOiTsZULchfAdUD2Ue3yQgVL5gs')
+    .pipe(
+      map(data => data.url)
+    )
+  }
+}
